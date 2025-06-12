a463859a2d60e1e01c8f9b378ee95f619bd75c0c
kasse <fatoukc195@gmail.com>
2025-06-12 02:58:09 +0200

    Ajout de API Kasse

---
+++ resources/js/api/AuthContext.jsx
@@ -0,0 +1,34 @@
+import React ,{ createContext, useContext, useState, useEffect } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (token) {
+        
+      setUser({ name: "Utilisateur" });
+    }
+  }, [token]);
+
+  const login = (token) => {
+    localStorage.setItem("token", token);
+    setToken(token);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
